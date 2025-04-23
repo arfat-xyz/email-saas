fabfa4fcfac59d6e2e402da6d6088e87fa50b617
arfat-xyz <arfatrahman08@gmail.com>
2025-04-23 16:14:44 +0600

    next-auth with nextjs-15.2 added

--- app/(un-protected-route)/login/page.tsx
+++ app/(un-protected-route)/login/page.tsx
@@ -2,13 +2,25 @@ import { signIn } from "@/auth";
 
 export default function SignIn() {
   return (
-    <form
-      action={async () => {
-        "use server";
-        await signIn("github");
-      }}
-    >
-      <button type="submit">Signin with GitHub</button>
-    </form>
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <div className="bg-white p-8 rounded-lg shadow-lg w-full max-w-sm">
+        <h2 className="text-2xl font-semibold text-center text-gray-800 mb-6">
+          Sign in to Your Account
+        </h2>
+        <form
+          action={async () => {
+            "use server";
+            await signIn("github");
+          }}
+        >
+          <button
+            type="submit"
+            className="w-full py-3 px-6 bg-blue-600 text-white rounded-lg shadow-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50"
+          >
+            Sign in with GitHub
+          </button>
+        </form>
+      </div>
+    </div>
   );
 }
